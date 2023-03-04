5b0cc4734a0aacbec5671b8c545371f558568d5a
IsabelM <Isabelvmn93@gmail.com>
2023-03-04 20:31:30 +0000

    trabajando en sentencias

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/ListadoSentencias.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/ListadoSentencias.java
@@ -1,5 +1,38 @@
 package compiler.syntax.nonTerminal;
 
+import java.util.ArrayList;
+
 public class ListadoSentencias extends NonTerminal {
+	
+	private ArrayList<Sentencia> listaSentencias;
+	private Boolean devuelve;
+	
+	public ListadoSentencias() {
+		this.listaSentencias = new ArrayList<>();
+		this.devuelve = false;
+	}
+
+	public ArrayList<Sentencia> getListaSentencias() {
+		return listaSentencias;
+	}
+
+	public void setListaSentencias(ArrayList<Sentencia> listaSentencias) {
+		this.listaSentencias = listaSentencias;
+	}
+
+	public Boolean getDevuelve() {
+		return devuelve;
+	}
+
+	public void setDevuelve(Boolean devuelve) {
+		this.devuelve = devuelve;
+	}
+	
+	public void add(Sentencia sentencia) {
+		listaSentencias.add(sentencia);
+	}
+	
+	
+	
 
 }
