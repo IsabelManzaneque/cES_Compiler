608372816928c8c4addda6cc2518b3caa5e64684
IsabelM <Isabelvmn93@gmail.com>
2023-03-04 18:15:19 +0000

    sorted ref non terminal

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/Ref.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/Ref.java
@@ -2,19 +2,35 @@ package compiler.syntax.nonTerminal;
 
 public class Ref extends NonTerminal {
 	
-	private String name;
-
-	public Ref(String name) {
-		this.name = name;
+	private String id;
+	private AccesoVector accesoVector;
+	
+	public Ref(String id) {
+		this.id = id;
+		this.accesoVector = null;
+	}
+	
+	public Ref(AccesoVector accesoVector) {
+		this.id = null;
+		this.accesoVector = accesoVector;
 	}
 
-	public String getName() {
-		return name;
+	public String getId() {
+		return id;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setId(String id) {
+		this.id = id;
 	}
+
+	public AccesoVector getAccesoVector() {
+		return accesoVector;
+	}
+
+	public void setAccesoVector(AccesoVector accesoVector) {
+		this.accesoVector = accesoVector;
+	}
+	
 	
 	
 	
