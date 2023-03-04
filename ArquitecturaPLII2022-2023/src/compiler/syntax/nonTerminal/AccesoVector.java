8d42c853d085b7fa5767bca619375034268a1cb6
IsabelM <Isabelvmn93@gmail.com>
2023-03-04 12:38:44 +0000

    semantico accesoVector completa

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/AccesoVector.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/AccesoVector.java
@@ -1,5 +1,38 @@
 package compiler.syntax.nonTerminal;
 
 public class AccesoVector extends NonTerminal {
+	
+	private String name;
+	private Expresion expresion;
+	
+	
+	public AccesoVector(String name, Expresion expresion) {
+		this.name = name;
+		this.expresion = expresion;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Expresion getExpresion() {
+		return expresion;
+	}
+
+
+	public void setExpresion(Expresion expresion) {
+		this.expresion = expresion;
+	}
+	
+	
+	
+	
 
 }
