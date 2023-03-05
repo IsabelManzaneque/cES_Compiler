71402db0fc2caca0c02bfdb938aa7bcc1745ff51
IsabelM <Isabelvmn93@gmail.com>
2023-03-05 15:58:22 +0000

    trabajando en las sentencias

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SentenciaLlamadaFuncion.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SentenciaLlamadaFuncion.java
@@ -1,5 +1,22 @@
 package compiler.syntax.nonTerminal;
 
-public class SentenciaLlamadaFuncion extends NonTerminal {
+public class SentenciaLlamadaFuncion extends Sentencia {
+	
+	private LlamadaFuncion llamadaFuncion;
+
+	public SentenciaLlamadaFuncion(LlamadaFuncion llamadaFuncion) {
+		super();
+		this.llamadaFuncion = llamadaFuncion;
+	}
+
+	public LlamadaFuncion getLlamadaFuncion() {
+		return llamadaFuncion;
+	}
+
+	public void setLlamadaFuncion(LlamadaFuncion llamadaFuncion) {
+		this.llamadaFuncion = llamadaFuncion;
+	}
+	
+	
 
 }
--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SentenciaMientras.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SentenciaMientras.java
@@ -1,5 +1,32 @@
 package compiler.syntax.nonTerminal;
 
-public class SentenciaMientras extends NonTerminal {
+public class SentenciaMientras extends Sentencia {
+	
+	private Expresion expresion;
+	private Sentencia sentencia;
+	
+	public SentenciaMientras(Expresion expresion, Sentencia sentencia) {
+		super();
+		this.expresion = expresion;
+		this.sentencia = sentencia;
+	}
+
+	public Expresion getExpresion() {
+		return expresion;
+	}
+
+	public void setExpresion(Expresion expresion) {
+		this.expresion = expresion;
+	}
+
+	public Sentencia getSentencia() {
+		return sentencia;
+	}
+
+	public void setSentencia(Sentencia sentencia) {
+		this.sentencia = sentencia;
+	}
+	
+	
 
 }
