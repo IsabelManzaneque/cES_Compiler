c7a24f3f9d480ff0dcb195154511b6d667845fd7
IsabelM <Isabelvmn93@gmail.com>
2023-03-05 18:30:13 +0000

    terminada sentencia bloque?

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SeccionVariables.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SeccionVariables.java
@@ -1,5 +1,28 @@
 package compiler.syntax.nonTerminal;
 
+import java.util.ArrayList;
+
 public class SeccionVariables extends NonTerminal {
+	
+	private ArrayList<DeclaracionVariable> listaVariables;
+
+	public SeccionVariables() {
+		super();
+		this.listaVariables = new ArrayList<>();
+	}
+
+	public ArrayList<DeclaracionVariable> getListaVariables() {
+		return listaVariables;
+	}
+
+	public void setListaVariables(ArrayList<DeclaracionVariable> listaVariables) {
+		this.listaVariables = listaVariables;
+	}
+	
+	public void add(DeclaracionVariable declaracionVariable) {
+		listaVariables.add(declaracionVariable);
+	}
+	
+	
 
 }
