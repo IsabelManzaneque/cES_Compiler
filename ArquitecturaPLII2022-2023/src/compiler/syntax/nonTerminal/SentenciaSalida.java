a3afa541583b777cfe3c3b735fe6d6e97b7a53d7
IsabelM <Isabelvmn93@gmail.com>
2023-03-05 17:23:36 +0000

    sentencias salida

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SentenciaSalida.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SentenciaSalida.java
@@ -1,5 +1,41 @@
 package compiler.syntax.nonTerminal;
 
 public class SentenciaSalida extends NonTerminal {
+	
+	private String opcionEscribe;
+	private Expresion opcionEscribeEntero;
+	
+	public SentenciaSalida() {
+		super();
+		this.opcionEscribe = null;
+		this.opcionEscribeEntero = null;
+	}
+	
+	public SentenciaSalida(String opcionEscribe) {
+		super();
+		this.opcionEscribe = opcionEscribe;
+		this.opcionEscribeEntero = null;
+	}
+	public SentenciaSalida(Expresion opcionEscribeEntero) {
+		super();
+		this.opcionEscribe = null;
+		this.opcionEscribeEntero = opcionEscribeEntero;
+	}
+	public String getOpcionEscribe() {
+		return opcionEscribe;
+	}
+	public void setOpcionEscribe(String opcionEscribe) {
+		this.opcionEscribe = opcionEscribe;
+	}
+	public Expresion getOpcionEscribeEntero() {
+		return opcionEscribeEntero;
+	}
+	public void setOpcionEscribeEntero(Expresion opcionEscribeEntero) {
+		this.opcionEscribeEntero = opcionEscribeEntero;
+	}
+	
+	
+	
+	
 
 }
