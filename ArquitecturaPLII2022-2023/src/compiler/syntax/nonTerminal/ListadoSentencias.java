495300ccea63a21e8d9d7901012507d9cd4c8993
IsabelM <Isabelvmn93@gmail.com>
2023-03-12 12:17:22 +0000

    funciones devuelve

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/ListadoSentencias.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/ListadoSentencias.java
@@ -2,15 +2,20 @@ package compiler.syntax.nonTerminal;
 
 import java.util.ArrayList;
 
+import es.uned.lsi.compiler.semantic.type.TypeIF;
+
 public class ListadoSentencias extends NonTerminal {
 	
 	private ArrayList<Sentencia> listaSentencias;
 	private Boolean devuelve;
+	private TypeIF tipoDevuelto;
 	
 	public ListadoSentencias() {
 		super();
 		this.listaSentencias = new ArrayList<>();
 		this.devuelve = false;
+		this.tipoDevuelto = null;
+		
 	}
 
 	public ArrayList<Sentencia> getListaSentencias() {
@@ -29,9 +34,19 @@ public class ListadoSentencias extends NonTerminal {
 		this.devuelve = devuelve;
 	}
 	
+	
+	public TypeIF getTipoDevuelto() {
+		return tipoDevuelto;
+	}
+
+	public void setTipoDevuelto(TypeIF tipoDevuelto) {
+		this.tipoDevuelto = tipoDevuelto;
+	}
+
 	public void add(Sentencia sentencia) {
 		listaSentencias.add(sentencia);
 	}
+	
 	
 	
 	
