ad422e82691503e8c5fa4b1ee126b0a7085020e8
IsabelM <Isabelvmn93@gmail.com>
2023-03-12 11:03:52 +0000

    propagacion de atributos

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/Funcion.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/Funcion.java
@@ -1,12 +1,44 @@
 package compiler.syntax.nonTerminal;
 
+import es.uned.lsi.compiler.semantic.type.TypeIF;
+
 public class Funcion extends NonTerminal {
 	
+	private TypeIF type;
+	private String name;
+	private SeccionParametros seccionParametros;
 	private CuerpoFuncion cuerpoFuncion;
-
-	public Funcion(CuerpoFuncion cuerpoFuncion) {
+	
+	public Funcion(TypeIF type, String name, SeccionParametros seccionParametros, CuerpoFuncion cuerpoFuncion) {
 		super();
+		this.type = type;
+		this.name = name;
+		this.seccionParametros = seccionParametros;
 		this.cuerpoFuncion = cuerpoFuncion;
+	}
+
+	public TypeIF getType() {
+		return type;
+	}
+
+	public void setType(TypeIF type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public SeccionParametros getSeccionParametros() {
+		return seccionParametros;
+	}
+
+	public void setSeccionParametros(SeccionParametros seccionParametros) {
+		this.seccionParametros = seccionParametros;
 	}
 
 	public CuerpoFuncion getCuerpoFuncion() {
@@ -17,6 +49,6 @@ public class Funcion extends NonTerminal {
 		this.cuerpoFuncion = cuerpoFuncion;
 	}
 	
-	
+		
 	
 }
--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SeccionParametros.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/SeccionParametros.java
@@ -1,5 +1,22 @@
 package compiler.syntax.nonTerminal;
 
 public class SeccionParametros extends NonTerminal {
+	
+	private DeclaracionParametros declaracionParametros;
+
+	public SeccionParametros(DeclaracionParametros declaracionParametros) {
+		super();
+		this.declaracionParametros = declaracionParametros;
+	}
+
+	public DeclaracionParametros getDeclaracionParametros() {
+		return declaracionParametros;
+	}
+
+	public void setDeclaracionParametros(DeclaracionParametros declaracionParametros) {
+		this.declaracionParametros = declaracionParametros;
+	}
+	
+	
 
 }
