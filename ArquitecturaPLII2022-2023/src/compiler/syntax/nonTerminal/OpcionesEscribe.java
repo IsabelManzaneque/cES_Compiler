eb266e83ad2d9ab9aa51d6520dbcc9792b2e397f
IsabelM <Isabelvmn93@gmail.com>
2023-03-18 20:53:46 +0000

    mas codigo intermedio

--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/OpcionesEscribe.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/OpcionesEscribe.java
@@ -1,5 +1,22 @@
 package compiler.syntax.nonTerminal;
 
 public class OpcionesEscribe extends NonTerminal {
+	
+	private String opciones;
+
+	public OpcionesEscribe(String opciones) {
+		super();
+		this.opciones = opciones;
+	}
+
+	public String getOpciones() {
+		return opciones;
+	}
+
+	public void setOpciones(String opciones) {
+		this.opciones = opciones;
+	}
+	
+	
 
 }
--- ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/OpcionesEscribeEnt.java
+++ ArquitecturaPLII2022-2023/src/compiler/syntax/nonTerminal/OpcionesEscribeEnt.java
@@ -1,5 +1,22 @@
 package compiler.syntax.nonTerminal;
 
 public class OpcionesEscribeEnt extends NonTerminal {
+	
+	private Expresion opciones;
 
+	public OpcionesEscribeEnt(Expresion opciones) {
+		super();
+		this.opciones = opciones;
+	}
+
+	public Expresion getOpciones() {
+		return opciones;
+	}
+
+	public void setOpciones(Expresion opciones) {
+		this.opciones = opciones;
+	}
+	
+	
+	
 }
